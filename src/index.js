b2fbfa8695912fc02197aadaa4894282ec425b82
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-13 23:11:37 +0530

    code-added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
+const roundTo = require("round-to");
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector')
+const { connection } = require("./connector");
 
+const { data } = require("./data");
+app.get("/totalRecovered", (req, res) => {
+  let recoveredCount = 0;
+  data.map((item) => {
+    recoveredCount += item.recovered;
+  });
+  res.send({
+    data: {
+      _id: "total",
+      recovered: recoveredCount
+    }
+  });
+  return;
+});
 
+app.get("/totalActive", (req, res) => {
+  let activeCount = 0;
+  data.map((item) => {
+    activeCount += item.infected - item.recovered;
+  });
+  res.send({
+    data: {
+      _id: "total",
+      active: activeCount
+    }
+  });
+  return;
+});
 
+app.get("/totalDeath", (req, res) => {
+  let deathCount = 0;
+  data.map((item) => {
+    deathCount += item.death;
+  });
+  res.send({
+    data: {
+      _id: "total",
+      death: deathCount
+    }
+  });
+  return;
+});
 
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
