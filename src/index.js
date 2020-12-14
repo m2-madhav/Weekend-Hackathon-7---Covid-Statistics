df360aae8914252a6b4c1b9726dedd22e264e43f
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-14 09:37:37 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -9,70 +9,7 @@ app.use(express.json());
 const { connection } = require("./connector");
 const data = require("./data");
 
-let recoveredPatients = 0;
-let totalActiveCases = 0;
-let totalDeaths = 0;
-let rateValue = [];
-let mortalityValue = [];
-
-for (let i = 0; i < data.length; i++) {
-  recoveredPatients += data[i].recovered;
-  totalActiveCases += data[i].infected - data[i].recovered;
-  totalDeaths += data[i].death;
-  rateValue[i] = parseFloat(
-    ((data[i].infected - data[i].recovered) / data[i].infected).toFixed(5)
-  );
-  mortalityValue[i] = parseFloat((data[i].death / data[i].infected).toFixed(5));
-}
-
-app.get("/totalRecovered", (req, res) => {
-  let obj = { data: { _id: "total", recovered: recoveredPatients } };
-  res.send(obj);
-});
-
-app.get("/totalActive", (req, res) => {
-  let obj = { data: { _id: "total", active: totalActiveCases } };
-  res.send(obj);
-});
-
-app.get("/totalDeath", (req, res) => {
-  let obj = { data: { _id: "total", death: totalDeaths } };
-  res.send(obj);
-});
-
-let hotspotStatesArr = [];
-for (let i = 0; i < data.length; i++) {
-  if (rateValue[i] > 0.1) {
-    let newObj = {
-      state: data[i].state,
-      rate: rateValue[i]
-    };
-    hotspotStatesArr.push(newObj);
-  }
-}
-
-app.get("/hotspotStates", (req, res) => {
-  let obj = { data: hotspotStatesArr };
-  res.send(obj);
-});
-
-let healthyStatesArr = [];
-for (let i = 0; i < data.length; i++) {
-  if (mortalityValue[i] < 0.005) {
-    let newObj = {
-      state: data[i].state,
-      mortality: mortalityValue[i]
-    };
-    healthyStatesArr.push(newObj);
-  }
-}
-
-app.get("/healthyStates", (req, res) => {
-  let obj = { data: healthyStatesArr };
-  res.send(obj);
-});
-
-/*let ob = data;
+let ob = data;
 let recoveredPatients = 0;
 let totalActiveCases = 0;
 let totalDeaths = 0;
@@ -139,7 +76,7 @@ for (let i = 0; i < ob.data.length; i++) {
 app.get("/healthyStates", (req, res) => {
   let obj = { data: healthyStatesArr };
   res.send(obj);
-});*/
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
