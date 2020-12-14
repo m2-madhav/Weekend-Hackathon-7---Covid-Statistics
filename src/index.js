e4f6d208a5e28ec4771de97d3e2012e680508588
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-14 07:23:36 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 const { connection } = require("./connector");
 
 const { data } = require("./data");
+
 app.get("/totalRecovered", (req, res) => {
   let recoveredCount = 0;
   data.map((item) => {
@@ -24,21 +25,17 @@ app.get("/totalRecovered", (req, res) => {
   return;
 });
 
-app.get("/totalActive", (req, res) => {
-  let activeCount = 0;
-  data.map((item) => {
-    activeCount += item.infected - item.recovered;
-  });
-  res.send({
-    data: {
-      _id: "total",
-      active: activeCount
-    }
-  });
-  return;
-});
-
 app.get("/totalDeath", (req, res) => {
+  /*collection_connection.aggregate(
+      [
+          {
+              $group:{
+                  _id:"total",
+                  death:{$sum:"$death"}
+              }
+          }
+      ]
+  ).then((p)=>res.send(p));*/
   let deathCount = 0;
   data.map((item) => {
     deathCount += item.death;
@@ -47,6 +44,34 @@ app.get("/totalDeath", (req, res) => {
     data: {
       _id: "total",
       death: deathCount
+    }
+  });
+  return;
+});
+
+app.get("/totalActive", (req, res) => {
+  /* collection_connection.aggregate(
+      [
+          {
+              $group:{
+                  _id:"total",
+                  active:{
+                      $sum: {$subtract:["$infected","$recovered"]}
+                  }
+              }
+          }
+      ]
+  ).then((p)=>res.send(p));
+  */
+
+  let totalActive = 0;
+  data.map((item) => {
+    totalActive += item.infected - item.recovered;
+  });
+  res.send({
+    data: {
+      _id: "total",
+      totalActive: totalActive
     }
   });
   return;
